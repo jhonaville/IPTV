16d30723287e950ddd59fb665ffae2acb399d43e
Jhonathan Villegas of EAM <jvillegas.eam@gmail.com>
2023-08-03 13:57:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
-  let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+// Obtener el elemento de video y el lienzo
+let video = document.getElementById('video');
+let canvas = document.getElementById('canvas');
+let context = canvas.getContext('2d');
 
-  // Para escanear el código de barras
-  scanner.addListener('scan', function (content) {
-    alert('Código de barras escaneado: ' + content);
-    // Aquí puedes manejar el contenido del código de barras como desees.
-  });
+// Crear un escáner
+let scanner = new Instascan.Scanner({ video: video });
 
-  // Para escanear el código QR
-  scanner.addListener('content', function (content) {
-    alert('Código QR escaneado: ' + content);
-    // Aquí puedes manejar el contenido del código QR como desees.
-  });
+// Escanear el código de barras o QR
+scanner.addListener('scan', function(content) {
+  alert('Código escaneado: ' + content);
+});
 
-  // Comenzar a la escucha de la cámara
-  Instascan.Camera.getCameras().then(function (cameras) {
-    if (cameras.length > 0) {
-      scanner.start(cameras[0]);
-    } else {
-      console.error('No se encontró cámara en el dispositivo.');
-    }
-  }).catch(function (e) {
-    console.error(e);
-  });
+// Iniciar la cámara
+Instascan.Camera.getCameras().then(function(cameras) {
+  if (cameras.length > 0) {
+    scanner.start(cameras[0]);
+  } else {
+    console.error('No se encontraron cámaras en el dispositivo.');
+  }
+}).catch(function(error) {
+  console.error(error);
 });
